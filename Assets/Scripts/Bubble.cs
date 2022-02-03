42473365ddb8bd937d8b27cfd527d44e14b497a3
chelGomes <mgandrade2014@outlook.com>
2022-02-03 01:11:00 -0300

    implementacao das classe do jogo

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Bolha : MonoBehaviour{
+    public float alcance = 0.7f;
+    public float desvio = 0.51f;
+
+    public bool ehFixo;
+    public bool ehConectado;
+
+    public CorBolha corBolha;
+
+    private void OnCollisionEnter2D(Collision2D colisao){
+        if (colisao.gameObject.tag == "Bolha" && colisao.gameObject.GetComponent<Bubble>().ehFixo){
+            if (!ehFixo){
+                Colidiu();
+            }
+        }
+
+        if (colisao.gameObject.tag == "Limite"){
+            if (!ehFixo){
+                Colidiu();
+            }
+        }
+    }
+
+    private void Colidiu(){
+        var rb = GetComponent<Rigidbody2D>();
+        Destroy(rb);
+        ehFixo = true;
+        LevelManager.instance.SetAsBubbleAreaChild(transform);
+        GameManager.instance.ProcessTurn(transform);
+    }
+
+    public List<Transform> GetNeighbors() {
+        List<RaycastHit2D> hits = new List<RaycastHit2D>();
+        List<Transform> neighbors = new List<Transform>();
+
+        hits.Add(Physics2D.Raycast(new Vector2(transform.position.x - desvio, transform.position.y), Vector3.left, alcance));
+        hits.Add(Physics2D.Raycast(new Vector2(transform.position.x + desvio, transform.position.y), Vector3.right, alcance));
+        hits.Add(Physics2D.Raycast(new Vector2(transform.position.x - desvio, transform.position.y + desvio), new Vector2(-1f, 1f), alcance));
+        hits.Add(Physics2D.Raycast(new Vector2(transform.position.x - desvio, transform.position.y - desvio), new Vector2(-1f, -1f), alcance));
+        hits.Add(Physics2D.Raycast(new Vector2(transform.position.x + desvio, transform.position.y + desvio), new Vector2(1f, 1f), alcance));
+        hits.Add(Physics2D.Raycast(new Vector2(transform.position.x + desvio, transform.position.y - desvio), new Vector2(1f, -1f), alcance));
+
+        foreach(RaycastHit2D hit in hits){
+            if(hit.colidir != null && hit.transform.tag.Equals("Bolha")){
+                neighbors.Add(hit.transform);
+            }
+        }
+
+        return neighbors;
+    }
+
+    void tornouInvisivel(){
+        Destroy(gameObject);
+    }
+
+    private void Selecionado(){
+        Gizmos.cor = Cor.red;
+    }
+
+    public enum CorBolha{
+        BLUE, YELLOW, RED, GREEN
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    #region Singleton
+    public static GameManager instance;
+
+    private void Awake(){
+        if (instance == null){
+            instance = this;
+        }
+    }
+    #endregion
+
+    public Shooter shootScript;
+    public Transform pointerToLastLine;
+
+    private int sequenceSize = 3;
+    [SerializeField]
+    private List<Transform> bubbleSequence;
+
+    void Start() {
+        bubbleSequence = new List<Transform>();
+
+        LevelManager.instance.GenerateLevel();
+        
+        shootScript.canShoot = true;
+        shootScript.CreateNextBubble();
+    }
+
+    void Update(){
+        if (shootScript.canShoot
+            && Input.GetMouseButtonUp(0)
+            && (Camera.main.ScreenToWorldPoint(Input.mousePosition).y > shootScript.transform.position.y)){
+            shootScript.canShoot = false;
+            shootScript.Shoot();
+        }
+    }
+
+    public void ProcessTurn(Transform currentBubble){
+        bubbleSequence.Clear();
+        CheckBubbleSequence(currentBubble);
+
+        if(bubbleSequence.Count >= sequenceSize){
+            DestroyBubblesInSequence();
+            DropDisconectedBubbles();
+        }
+
+        LevelManager.instance.UpdateListOfBubblesInScene();
+
+        shootScript.CreateNextBubble();
+        shootScript.canShoot = true;
+    }
+
+    private void CheckBubbleSequence(Transform currentBubble){
+        bubbleSequence.Add(currentBubble);
+
+        Bolha bubbleScript = currentBubble.GetComponent<Bolha>();
+        List<Transform> neighbors = bubbleScript.GetNeighbors();
+
+        foreach(Transform t in neighbors){
+            if (!bubbleSequence.Contains(t)) {
+
+                Bolha bScript = t.GetComponent<Bolha>();
+
+                if (bScript.corBolha == bubbleScript.corBolha) {
+                    CheckBubbleSequence(t);
+                }
+            }
+        }
+    }
+
+    private void DestroyBubblesInSequence(){
+        foreach(Transform t in bubbleSequence){
+            Destroy(t.gameObject);
+        }
+    }
+
+    private void DropDisconectedBubbles(){
+        SetAllBubblesConnectionToFalse();
+        SetConnectedBubblesToTrue();
+        SetGravityToDisconectedBubbles();
+    }
+
+    #region Drop Disconected Bubbles
+    private void SetAllBubblesConnectionToFalse(){
+        foreach (Transform bubble in LevelManager.instance.bubblesArea){
+            bubble.GetComponent<Bolha>().ehConectado = false;
+        }
+    }
+
+    private void SetConnectedBubblesToTrue() {
+        bubbleSequence.Clear();
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(pointerToLastLine.position, pointerToLastLine.right, 15f);
+
+        for (int i = 0; i < hits.Length; i++){
+            if (hits[i].transform.gameObject.tag.Equals("Bolha"))
+                SetNeighboursConnectionToTrue(hits[i].transform);
+        }
+    }
+
+    private void SetNeighboursConnectionToTrue(Transform bolha){
+        Bolha bubbleScript = bubble.GetComponent<Bubble>();
+        bubbleScript.ehConectado = true;
+        bubbleSequence.Add(bolha);
+
+        foreach(Transform t in bubbleScript.GetNeighbors()){
+            if(!bubbleSequence.Contains(t)){
+                SetNeighboursConnectionToTrue(t);
+            }
+        }
+    }
+
+    private void SetGravityToDisconectedBubbles(){
+        foreach (Transform bolha in LevelManager.instance.bubblesArea){
+            if (!bolha.GetComponent<Bolha>().ehConectado){
+                bolha.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+                if(!bolha.GetComponent<Rigidbody2D>()){
+                    Rigidbody2D rb2d = bolha.gameObject.AddComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+                }       
+            }
+        }
+    }
+    #endregion
+}
